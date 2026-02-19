e4a03510c083f9d74718739311da2d22f8d424e4
mattmunich <mattmunich005@gmail.com>
2026-02-19 10:05:13 +0100

    - Add /iceboatracing command for reloading and some info abt the plugin - Make the onEnable easier to read

--- src/main/java/fr/mattmunich/iceBoatRacing/IBRCommand.java
+++ src/main/java/fr/mattmunich/iceBoatRacing/IBRCommand.java
@@ -2,29 +2,66 @@ package fr.mattmunich.iceBoatRacing;
 
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.scoreboard.*;
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.List;
+
+import static fr.mattmunich.iceBoatRacing.Messages.getMessage;
 
 public class IBRCommand implements BasicCommand {
+
+    private final Main main;
+
+    private final Messages messages;
+
+    public IBRCommand(Main main, Messages messages) {
+        this.main = main;
+        this.messages = messages;
+    }
+
     @Override
-    public void execute(CommandSourceStack commandSourceStack, String[] strings) {
+    public void execute(@NonNull CommandSourceStack source, String @NonNull [] args) {
+        CommandSender s = source.getSender();
+        if(args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+            if(!s.hasPermission("iceBoatRacing.reload")) return;
+            s.sendMessage(getMessage("prefix") + "§b§oReloading...");
+            main.loadConfigs();
+            messages.reload();
+            main.registerScoreboard();
+            s.sendMessage(getMessage("prefix") + "§bDone reloading!");
+        } else if (args.length == 1 && args[0].equalsIgnoreCase("info")) {
+            s.sendMessage(getMessage("prefix") + """
+                    §3---§b Ice Boat Racing §3---
+                    §3Created by§b mattmunich
+                    §3Created for §bGP 2026§3 of§b Mini Jeux Entre Potes
+                    """);
+        } else {
+            s.sendMessage(getMessage("prefix") + """
+                    §3---§b /iceboatracing §3---
+                    §3- reload §7- §bReloads the plugin
+                    §3- info   §7- §bSome basic info
+                    """);
+        }
 
     }
 
     @Override
-    public Collection<String> suggest(CommandSourceStack commandSourceStack, String[] args) {
-        return BasicCommand.super.suggest(commandSourceStack, args);
+    public @NonNull Collection<String> suggest(@NonNull CommandSourceStack commandSourceStack, String @NonNull [] args) {
+        return List.of("reload","info");
     }
 
     @Override
-    public boolean canUse(CommandSender sender) {
+    public boolean canUse(@NonNull CommandSender sender) {
         return BasicCommand.super.canUse(sender);
     }
 
     @Override
     public @Nullable String permission() {
-        return BasicCommand.super.permission();
+        return "iceBoatRacing.command";
     }
 }
--- src/main/java/fr/mattmunich/iceBoatRacing/Main.java
+++ src/main/java/fr/mattmunich/iceBoatRacing/Main.java
@@ -1,10 +1,7 @@
 package fr.mattmunich.iceBoatRacing;
 
 import fr.mattmunich.iceBoatRacing.cars.CarCommand;
-import fr.mattmunich.iceBoatRacing.cars.CarListener;
 import fr.mattmunich.iceBoatRacing.cars.CarManager;
-import fr.mattmunich.iceBoatRacing.listeners.Connection;
-import fr.mattmunich.iceBoatRacing.race.RaceListener;
 import fr.mattmunich.iceBoatRacing.race.RaceData;
 import fr.mattmunich.iceBoatRacing.livescoreboard.checkpoint.CheckpointCommand;
 import fr.mattmunich.iceBoatRacing.livescoreboard.checkpoint.CheckpointManager;
@@ -15,8 +12,6 @@ import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
@@ -43,28 +38,62 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         log("Enabling plugin...");
 
+        loadConfigs();
+
+        loadMessages();
+
+        loadCheckpoints();
+
+        loadCars();
+
+        loadRaceManager();
+
+        if (registerScoreboard()) return;
+
+        registerCommands();
+
+        log("Done enabling plugin!");
+
+        loadLuckPerms();
+    }
+
+
+    public void loadConfigs() {
         log("Configuring config files");
         saveDefaultConfig();
         saveResource("lang/en_US.yml", false);
         saveResource("lang/fr_FR.yml", false);
         log("Done configuring config files!");
+    }
 
+    private void loadMessages() {
+        log("Loading messages...");
+        messages = new Messages(this);
+        log("Done loading messages!");
+    }
+
+    private void loadCheckpoints() {
         log("Loading checkpoints...");
         saveDefaultConfig();
         checkpointManager = new CheckpointManager(this);
         checkpointManager.loadCheckpoints();
         log("Done loading checkpoints!");
+    }
 
+    private void loadCars() {
         log("Loading cars...");
-        saveDefaultConfig();
         carManager = new CarManager(this);
         carManager.loadCars();
         log("Done loading cars!");
+    }
 
+    private void loadRaceManager() {
         log("Loading race manager...");
         raceManager = new RaceManager(this,carManager);
         log("Done loading race manager!");
+    }
 
+    public boolean registerScoreboard() {
         log("Registering scoreboard...");
         ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
         Scoreboard scoreboard = scoreboardManager.getMainScoreboard();
@@ -78,35 +107,30 @@ public final class Main extends JavaPlugin {
             if(liveSidebar == null) {
                 log("Could not find live sidebar!");
                 Bukkit.getPluginManager().disablePlugin(this);
-                return;
+                return true;
             }
             liveSidebar.displayName(getMessage("noPrefix.liveSidebarTitle"));
         }
         if(liveSidebar == null) {
             log("Could not find live sidebar!");
             Bukkit.getPluginManager().disablePlugin(this);
-            return;
+            return true;
         }
         liveSidebar.setDisplaySlot(DisplaySlot.SIDEBAR);
         log("Done registering scoreboard!");
+        return false;
+    }
 
+    private void registerCommands() {
         log("Registering commands...");
+        registerCommand("iceboatracing", "Command to manage the plugin", List.of("ibr"), new IBRCommand(this,messages));
         registerCommand("checkpoint", "Command to manage checkpoints", new CheckpointCommand(checkpointManager,this));
         registerCommand("car", "Command to manage cars", new CarCommand(this,carManager));
         registerCommand("race", "Command to manage the race", new RaceCommand(this,raceManager));
         log("Done registering commands!");
+    }
 
-        log("Registering listeners...");
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(new CheckpointCommand(checkpointManager,this), this);
-        pm.registerEvents(new CarCommand(this,carManager), this);
-        pm.registerEvents(new Connection(this,carManager), this);
-        pm.registerEvents(new RaceListener(this,checkpointManager), this);
-        pm.registerEvents(new CarListener(this), this);
-        log("Done registering listeners!");
-
-        log("Done enabling plugin!");
-
+    private void loadLuckPerms() {
         Bukkit.getScheduler().runTask(this, () -> {
             log("Loading LuckPerms dependency");
             RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
@@ -118,6 +142,8 @@ public final class Main extends JavaPlugin {
             }
         });
     }
+
+
 
     @Override
     public void onDisable() {
