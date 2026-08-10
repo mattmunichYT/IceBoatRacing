14122e4a39fdfd6db3abcfe023a43710d98bcbc8
mattmunich <mattmunich005@gmail.com>
2026-08-10 11:20:59 +0200

    Now supports 1.21.5 and later

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
 }
 
 tasks.processResources {
